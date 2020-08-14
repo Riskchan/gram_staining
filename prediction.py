cf698e33e1ee491a8df4437d3ce68ecb1a6f7106
Akihiko Minami <minamiakihiko@gmail.com>
2020-08-14 20:42:37 +0900

    Tiny update

--- prediction.py
+++ prediction.py
@@ -20,7 +20,7 @@ img_width, img_height = 128, 128
 feature_dim = (img_width, img_height, 3)
 
 # Load model
-model = tf.keras.models.load_model("weights/smallcnn30.hdf5")
+model = tf.keras.models.load_model("weights/vgg16-finetuning30.hdf5")
 
 # Load image
 img = image.load_img(input_filename, target_size=(img_height, img_width))
@@ -32,4 +32,5 @@ x = x / 255.0
 
 # Prediction
 pred = model.predict(x)[0]
-print(pred)
+print("{}: {}%".format(classes[0], (1-pred) * 100.0))
+print("{}: {}%".format(classes[1], pred * 100.0))
