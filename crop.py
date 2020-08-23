9ecef4bba0f059943890002614aa694301d86820
Akihiko Minami <minamiakihiko@gmail.com>
2020-08-23 10:11:55 +0900

    Added cropping script

---
+++ crop.py
@@ -0,0 +1,21 @@
+import sys
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+input_filenames = sys.argv[1:]
+crop_dir = "./cropped"
+crop_width, crop_height = 256, 256
+
+for filename in input_filenames:
+    name = filename.split("/")[-1].split(".")[0]
+    img = Image.open(filename)
+    width, height = img.size
+    n_width = width // crop_width
+    n_height = height // crop_height
+    
+    for i in range(n_width):
+        for j in range(n_height):
+            img_crop = img.crop((i*crop_width, j*crop_height, (i+1)*crop_width, (j+1)*crop_height))
+            img_crop.save("{}/{}-{:03d}x{:03d}.png".format(crop_dir, name, i, j))
